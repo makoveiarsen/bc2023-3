5a96fdffc43400ef4eb1517877dc21847bce5b6e
bc2024-1 <arsenpro1213@gmail.com>
2024-10-10 11:24:32 +0300

    added main.js

---
+++ main.js
@@ -0,0 +1,54 @@
+const { program } = require("commander");
+const fs = require("fs");
+
+program
+  .option("-i, --input <path>", "path to data.json")
+  .option("-o, --output <path>", "path to output file")
+  .option("-d, --display", "display results in console");
+
+program.parse(process.argv);
+
+const options = program.opts();
+const input = options.input;
+const output = options.output;
+const display = options.display;
+
+if (!input) {
+  console.error("Please, specify input file");
+  return;
+}
+
+if (!fs.existsSync(input)) {
+  console.error("Cannot find input file");
+  return;
+}
+
+if (!output && !display) {
+  return;
+}
+
+const data = JSON.parse(
+  fs.readFileSync(input, {
+    encoding: "utf-8",
+    flag: "r",
+  })
+);
+
+const filteredTheLowestData = data.reduce((accumulator, currentElement) => {
+  return accumulator.value < currentElement.value
+    ? accumulator
+    : currentElement;
+});
+
+const result = `${filteredTheLowestData.txt}: ${filteredTheLowestData.value}`;
+
+if (display) {
+  console.log(result);
+}
+if (output) {
+  fs.writeFileSync(output, result, {
+    encoding: "utf-8",
+    flag: "w",
+    flush: true,
+  });
+}
